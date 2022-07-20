722fc3e905dfb745f7cc6eded95b70b15371a63a
Your Name <you@example.com>
2022-07-20 11:59:16 +0200

    fix: fix the json name totalSnacks

--- entity/zimpler.go
+++ entity/zimpler.go
@@ -5,5 +5,5 @@ type TopRate []StoreResp
 type StoreResp struct {
 	Name           string `json:"name"`
 	FavouriteSnack string `json:"favouriteSnack"`
-	TotalSnacks    int    `json:"totalSnack"`
+	TotalSnacks    int    `json:"totalSnacks"`
 }
